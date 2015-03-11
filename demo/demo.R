de645683f74b3e84c21f8cb7967a53062024b16f
Xiaoyu Lu <xlu@stats.ox.ac.uk>
2015-03-11 17:10:57 +0000

    change

--- demo/demo.R
+++ demo/demo.R
@@ -1,9 +1,10 @@
-
-
+# load("out.RData")
+# load("MSE.RData")
 ##simulation study
 #training set
 a1 = 0.9; a2 = -0.8; f = 0.1
 dat = c()
+set.seed(100)
 dat[1] = rnorm(1);dat[2] = rnorm(1)
 for (k in 2:1000) {
   dat[k+1] = a1*dat[k] + a2*dat[k-1] + sin(2*pi*f*k) + rnorm(1)*0.05
@@ -25,10 +26,6 @@ for (i in k0:(N_tilda+k0-1)){
   x_test[,i-k0+1] = dat[(i-lag+1):(i)]
 }
 
-# output from the commented codes below
-load("out.RData")
-load("MSE.RData")
-
 #plot step-ahead predictions
 step=25
 y_test = dat[(k0+step):(k0+N_tilda+step-1)]
@@ -39,14 +36,14 @@ legend("topright",c("predictive mean","measurements"),lwd=c(1,1),col=c(2,1),pch=
 
 #perform one to M-step ahead prediction and calculate the MSE
 M=30
-# MSE=c();out = matrix(,M,N_tilda)
-# for (i in 1:3) {
-#   res = MGP(dat,N,Q,num_gen,lag,max,min,Pc,Pm,x_test,i)
-#   out[i,] = res
-#   y_test = dat[(k0+i):(k0+N_tilda+i-1)]
-#   MSE[i] = sum((out[i,]-y_test)^2/N_tilda)
-#   print(i)
-# }
+MSE=c();out = matrix(,M,N_tilda)
+for (i in 4:M) {
+  res = MGP(dat,N,Q,num_gen,lag,max,min,Pc,Pm,x_test,i)
+  out[i,] = res
+  y_test = dat[(k0+i):(k0+N_tilda+i-1)]
+  MSE[i] = sum((out[i,]-y_test)^2/N_tilda)
+  print(i)
+}
 
 par(mfrow=c(3,3))
 for (i in 1:9){
@@ -60,10 +57,33 @@ par(mfrow=c(1,1))
 plot(MSE,xlab="step ahead",ylab="MSE");lines(MSE)
 
 #plot one to M step ahead predictions starting from the (j+k0+1)th entry
-j=79
+j=100
 plot((k0+j):(k0+M + j-1),dat[(k0+j):(k0+M + j-1)],xlab="step k",ylab="y(k)",main="M-step ahead prediction");
 lines((k0+j):(k0+M + j-1),dat[(k0+j):(k0+M + j-1)])
 lines((k0+j):(k0+M + j-1),out[,j][1:30],col=2)
 points((k0+j):(k0+M + j-1),out[,j][1:30],col=2,pch=2)
 legend("topright",c("predictive mean","measurements"),col=c(2,1),lwd=c(1,1),pch=c(2,1))
+
+# save(out,file="out.RData")
+# save(MSE,file="MSE.RData")
+
+
+MSE1 = c()
+for (j in 1:100){
+  MSE1[j] = sum((out[,j][1:M]-dat[(k0+j):(k0+M + j-1)])^2)/M
+}
+
+hist(MSE1,xlab="MSE",breaks=50,main="histogram of MSE for up to 30 steps ahead predictions")
+median(MSE1)
+
+
+
+
+
+
+
+
+
+
+
 
